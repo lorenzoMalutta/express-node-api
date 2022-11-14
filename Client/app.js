7d9828f8602530fc0cb02b10a916158ba3ab3352
joão lorenzo malutta haidar <lorenzo.malutta@gmail.com>
2022-11-14 18:20:31 -0300

    :wrench:  feat add database mongo, Post and Get method

--- Client/app.js
+++ Client/app.js
@@ -3,16 +3,48 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 const { randomUUID } = require('crypto');
-
+const mongoose = require('mongoose');
+const Person = require('./models/Person');
 // end config
 
 // start database
+const uri = "mongodb+srv://lorenzo:12345@cluster0.wba683h.mongodb.net/?retryWrites=true&w=majority";
 
-const products = [];
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log('Database connected');
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+connect();
 // end database
 
 // start routes
+
+app.get('/person', async (req, res) => {
+  const persons = await Person.find();
+  return res.send(persons);
+});
+
+app.post('/person', async (req, res) => {
+  const  { name, age, approved } = req.body;
+  const person = {
+    name,
+    age,
+    approved
+  }
+  
+  try {
+    // save person
+    await Person.create(person);
+    res.status(201).send('Person created');
+  } catch (error) {
+    console.log(error);
+  }
+}); 
 
 app.get('/', (req, res) => {
   return res.send('Home da página');
